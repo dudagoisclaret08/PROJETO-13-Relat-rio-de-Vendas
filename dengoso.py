bbfd66e40e01ee454cd204515604fd50c08edec1
dudagoisclaret08 <dudagois86@gmail.com>
2025-11-05 14:24:41 -0300

    Update dengoso.py

--- dengoso.py
+++ dengoso.py
@@ -1,6 +1,3 @@
-# PROJETO 13 — Relatório de Vendas
-
-# Criar uma lista para armazenar as vendas
 vendas = []
 
 # Loop para receber dados
@@ -23,17 +20,18 @@ while True:
     if continuar == 'n':
         break
 
-# --- Cálculos ---
+#  Cálculos 
 total_vendido = sum(v["total"] for v in vendas)
 media_faturamento = total_vendido / len(vendas)
 
 # Encontrar o produto mais vendido (maior quantidade)
 mais_vendido = max(vendas, key=lambda v: v["quantidade"])
 
-# --- Relatório Final ---
+#  Relatório Final 
 print("\n========== RELATÓRIO DE VENDAS ==========")
 print(f"Total de vendas registradas: {len(vendas)}")
 print(f"Faturamento total: R$ {total_vendido:.2f}")
 print(f"Média de faturamento por venda: R$ {media_faturamento:.2f}")
 print(f"Produto mais vendido: {mais_vendido['produto']} ({mais_vendido['quantidade']} unidades)")
 print("==========================================")
+
